c0e016cc10a74ea329f12b27fe4dc545fdb12f58
Emmanuel <myname@gmail.com>
2023-06-30 12:11:57 +0100

    Javascript

---
+++ javascript/indexscript.js
@@ -0,0 +1,39 @@
+console.log("hello world!");
+
+// let car = "Benz";
+// let myObj = {String: "Just continue", id:48576};
+// console.log("My car is", car, ". The object is: ", myObj);
+
+// let a = "Emmanuel"
+// let b = "Gerald"
+// let c = "Nigeria"
+// let d = "Sagittarius"
+
+// console.log("My fullname is", a, b, "and I am from", c, "and my starsign is", d);
+
+let a = "Range Rover"
+let b = "Evoque"
+
+console.log("My Favourite car is", a, "and the model is", b)
+console.log(`My favourite car is ${a}, and the model is, ${b}`);
+
+// let a = 'My name is Emmanuel'
+// console.log("I want %c Emmanuel", "color: orage; font-family:fantasy; font-style: Bold");
+
+// let a
+// let b = "123"
+// let c = 123
+// let d = true
+// let e = {a:"Javascript"}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+let tM = 4000
+let mP = 2348
+let tLeftP = tM - mP
+
+console.log(`The total bill is £${tM} the remaining amount of money is £${tLeftP}`);
