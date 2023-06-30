f4b9ce85b6687d95c0c181cbf9e17aae09b66fc8
Emmanuel <myname@gmail.com>
2023-06-30 16:57:04 +0100

    javascript

--- javascript/indexscript.js
+++ javascript/indexscript.js
@@ -11,11 +11,11 @@ console.log("hello world!");
 
 // console.log("My fullname is", a, b, "and I am from", c, "and my starsign is", d);
 
-let a = "Range Rover"
-let b = "Evoque"
+// let a = "Range Rover"
+// let b = "Evoque"
 
-console.log("My Favourite car is", a, "and the model is", b)
-console.log(`My favourite car is ${a}, and the model is, ${b}`);
+// console.log("My Favourite car is", a, "and the model is", b)
+// console.log(`My favourite car is ${a}, and the model is, ${b}`);
 
 // let a = 'My name is Emmanuel'
 // console.log("I want %c Emmanuel", "color: orage; font-family:fantasy; font-style: Bold");
@@ -32,8 +32,142 @@ console.log(`My favourite car is ${a}, and the model is, ${b}`);
 // console.log(d);
 // console.log(e);
 
-let tM = 4000
-let mP = 2348
-let tLeftP = tM - mP
+// let tM = 4000
+// let mP = 2348
+// let tLeftP = tM - mP
 
-console.log(`The total bill is £${tM} the remaining amount of money is £${tLeftP}`);
+// console.log(`The total bill is £${tM} the remaining amount of money is £${tLeftP}`);
+
+// let x=4 % 2
+// console.log(x);
+
+
+// for (let a = 100; a <= 200; a++) {
+//     console.log(a);
+// }
+
+// for(let a = 100; a<=200; a++){
+//     if(a%2==0){
+//         console.log(`-`);
+//     } else {
+//         console.log("*")
+//     }
+// }
+
+// let a = 100
+// while (a<=200){
+//     if (a%2==0){
+//         console.log(`-`)
+//     } else{
+//         console.log(`*`)
+//     }
+//     a++
+// }
+
+// let i =0
+// while (i<10){
+//     let j = 1
+//     while(j<=10){
+//         console.log(j);
+//         j++
+//     }
+//     i++
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     for(let j=1; j<=10; j++){
+//         console.log(j);
+//     }
+// }
+
+// let a = 5
+// switch (a) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log(`Friday`);
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+//     case 7:
+//         console.log('Sunday');
+//         break;
+
+//     default:
+//         console.log('Day is missing');
+//         break;
+// }
+
+// let now = new Date()
+// let day = now.getDay()
+// switch(day){
+//     case 0:
+//         console.log('Sunday');
+//         break
+//     case 6:
+//         console.log('Saturday');
+//         break
+//     case 1:
+//         console.log('Monday');
+//         break
+//     case 2:
+//         console.log('Tuesday');
+//         break
+//     case 3:
+//         console.log('Wednesday');
+//         break
+//     case 4:
+//         console.log('Thursday');
+//         break
+//     case 5:
+//         console.log('Friday');
+//         break
+//     default:
+//         console.log('No date');
+//         break
+// }
+
+// let rebel = {
+//     String: "Emma",
+//     list:[],
+//     number:20,
+//     subObject:{}
+// }
+// console.log(rebel.number);
+
+let objects = [
+    {type:'Dog', size: 'big', breed:'german', age:1},
+    {type:'Cat', size: 'big', breed: 'french', age:3},
+    {type:'Dog', size: 'small', breed:'shepherd', age:5}
+]
+
+// let d = []
+// d.push(objects)
+// console.log(d);
+
+for(let objjj of objects){
+    console.log(objjj.type)
+}
+
+let i = 0
+while (i < objects){
+    let currentObj = objetcs [i]
+}
+// for(let name in objects){
+//     for(let obj in objects[name]){
+//         console.log(`${obj}: ${objects[name][obj]}`)
+//     }
+// }
+
+// console.log(objects[0].type);
